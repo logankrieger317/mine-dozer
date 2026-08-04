4beb8fb47cf8d0feec33a400b59affb8f0acd535
Logan Krieger <37088551+logankrieger317@users.noreply.github.com>
2026-08-03 20:32:50 -0500

    Configure multi-page build for Frontier Trail

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/mine-dozer/',
-  build: { target: 'es2022' }
+  build: {
+    target: 'es2022',
+    rollupOptions: {
+      input: {
+        mineDozer: resolve(process.cwd(), 'index.html'),
+        frontierTrail: resolve(process.cwd(), 'frontier-trail/index.html'),
+      },
+    },
+  },
 });
